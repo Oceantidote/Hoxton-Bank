c85540c6a94de360ea3a541a99464ac32348288c
leonard percival <leonardpercival@gmail.com>
2019-08-21 14:14:10 +0100

    added in users controller, views and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def dashboard
+  end
+
+  def send_money
+  end
+
+  def create_transfer
+  end
+
+  def choose_beneficiary
+  end
+
+  def add_beneficiary
+  end
+
+  def account
+  end
+
+  def create_beneficiary
+  end
+
+  def sent_confirmation
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "dashboard", to: "users#dashboard"
+  get "send_money", to: "users#send_money"
+  get "dashboard", to: "users#dashboard"
+  post "create_transfer", to: "users#create_transfer"
+  get "add_beneficiary", to: "users#add_beneficiary"
+  post "create_beneficiary", to: "users#create_beneficiary"
+  get "sent_confirmation", to: "users#sent_confirmation"
+  get "account", to: "users#account"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
